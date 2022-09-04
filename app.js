64c9bc0ec9d18229581bfb0b32927256ca3d7b52
Vishal Sindham <vszb101@gmail.com>
2022-09-04 03:30:24 -0400

    intial commit: Check if your birthday is lucky

---
+++ app.js
@@ -0,0 +1,35 @@
+const dateOfBirth = document.querySelector("#dateOfBirth");
+const luckyNumber = document.querySelector("#luckyNumber");
+const checkBtn = document.querySelector("#checkBtn");
+const output = document.querySelector("#output");
+
+
+function compareValues(sum, luckyNumber) {
+  if (sum % luckyNumber === 0) {
+    output.innerText = "Your Birthday is Lucky ✨";
+    output.style.color = "yellow";
+  } else {
+    output.innerText = "Your birthday is not lucky ";
+    output.style.color = "red";
+  }
+}
+function checkBirthdayIsLucky() {
+  const dob = dateOfBirth.value;
+  const sum = calculateSum(dob);
+  console.log(sum);
+  if (sum && dob) {
+    compareValues(sum, luckyNumber.value);
+  } else {
+    output.innerText = "please fill all the boxes";
+  }
+}
+function calculateSum(dob) {
+  dob = dob.replaceAll("-", "");
+  var sum = 0;
+  for (var i = 0; i < dob.length; i++) {
+    sum = sum + Number(dob.charAt(i));
+  }
+  console.log(dob);
+  return sum;
+}
+checkBtn.addEventListener("click", checkBirthdayIsLucky);
